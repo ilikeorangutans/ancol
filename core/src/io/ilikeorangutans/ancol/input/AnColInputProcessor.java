6e0c7c9f6f12febedf142d8a4b0dd2a6bf5c434d
Jakob Külzer <jakob.kuelzer@gmail.com>
2014-12-13 16:05:04 -0500

    Added a few key bindings to test new simulation steps

--- core/src/io/ilikeorangutans/ancol/input/AnColInputProcessor.java
+++ core/src/io/ilikeorangutans/ancol/input/AnColInputProcessor.java
@@ -5,7 +5,10 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import io.ilikeorangutans.ancol.Point;
 import io.ilikeorangutans.ancol.game.cmd.BuildColonyCommand;
-import io.ilikeorangutans.ancol.game.event.CommandEvent;
+import io.ilikeorangutans.ancol.game.cmd.IdleCommand;
+import io.ilikeorangutans.ancol.game.cmd.ImproveTileCommand;
+import io.ilikeorangutans.ancol.game.cmd.event.CommandEvent;
+import io.ilikeorangutans.ancol.game.event.SimulateEntityEvent;
 import io.ilikeorangutans.ancol.map.ScreenToTile;
 import io.ilikeorangutans.ancol.map.ScrollEvent;
 import io.ilikeorangutans.ancol.move.MoveEvent;
@@ -20,112 +23,122 @@ import io.ilikeorangutans.ecs.Entity;
  */
 public class AnColInputProcessor implements InputProcessor {
 
-    private final EventBus bus;
-    private final ScreenToTile screenToTile;
-    private int lastX, lastY;
-    private Entity selectedEntity;
-    private boolean dragging = false;
+	private final EventBus bus;
+	private final ScreenToTile screenToTile;
+	private int lastX, lastY;
+	private Entity selectedEntity;
+	private boolean dragging = false;
 
-    public AnColInputProcessor(EventBus bus, ScreenToTile screenToTile) {
-        this.bus = bus;
-        this.screenToTile = screenToTile;
-    }
+	public AnColInputProcessor(EventBus bus, ScreenToTile screenToTile) {
+		this.bus = bus;
+		this.screenToTile = screenToTile;
+	}
 
-    @Subscribe
-    public void onSelectedEntity(SelectedEvent event) {
-        selectedEntity = event.entity;
-    }
+	@Subscribe
+	public void onSelectedEntity(SelectedEvent event) {
+		selectedEntity = event.entity;
+	}
 
-    @Override
-    public boolean keyDown(int keycode) {
+	@Override
+	public boolean keyDown(int keycode) {
 
-        switch (keycode) {
-            case Input.Keys.LEFT:
-                bus.fire(new ScrollEvent(-30, 0));
-                break;
-            case Input.Keys.RIGHT:
-                bus.fire(new ScrollEvent(30, 0));
-                break;
-            case Input.Keys.UP:
-                bus.fire(new ScrollEvent(0, -30));
-                break;
-            case Input.Keys.DOWN:
-                bus.fire(new ScrollEvent(0, 30));
-                break;
-            case Input.Keys.B:
-                bus.fire(new CommandEvent(new BuildColonyCommand()));
-                break;
-        }
+		switch (keycode) {
+			case Input.Keys.LEFT:
+				bus.fire(new ScrollEvent(-30, 0));
+				break;
+			case Input.Keys.RIGHT:
+				bus.fire(new ScrollEvent(30, 0));
+				break;
+			case Input.Keys.UP:
+				bus.fire(new ScrollEvent(0, -30));
+				break;
+			case Input.Keys.DOWN:
+				bus.fire(new ScrollEvent(0, 30));
+				break;
+			case Input.Keys.B:
+				bus.fire(new CommandEvent(new BuildColonyCommand()));
+				break;
+			case Input.Keys.P:
+				bus.fire(new CommandEvent(new ImproveTileCommand()));
+				break;
+			case Input.Keys.SPACE:
+				bus.fire(new CommandEvent(new IdleCommand()));
+				break;
+			case Input.Keys.ENTER:
+				bus.fire(new SimulateEntityEvent(selectedEntity));
+				break;
 
-        return false;
-    }
+		}
 
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public boolean keyTyped(char character) {
-        if (character == 'q') {
-            Gdx.app.exit();
-            return true;
-        }
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
 
-        return false;
-    }
+	@Override
+	public boolean keyTyped(char character) {
+		if (character == 'q') {
+			Gdx.app.exit();
+			return true;
+		}
 
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        final boolean wasDragging = dragging;
-        dragging = false;
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
 
-        final Point destination = screenToTile.screenToTile(screenX, screenY);
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		final boolean wasDragging = dragging;
+		dragging = false;
 
-        if ((button == 0 && wasDragging) || button == 1 && !wasDragging) {
-            bus.fire(new MoveEvent(destination));
-            return true;
-        } else if (button == 0) {
-            bus.fire(new SelectEvent(destination.x, destination.y));
-            return true;
-        }
+		final Point destination = screenToTile.screenToTile(screenX, screenY);
 
-        return false;
-    }
+		if ((button == 0 && wasDragging) || button == 1 && !wasDragging) {
+			bus.fire(new MoveEvent(destination));
+			return true;
+		} else if (button == 0) {
+			bus.fire(new SelectEvent(destination.x, destination.y));
+			return true;
+		}
 
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
 
-        dragging = true;
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
 
-        if (Gdx.input.isButtonPressed(1)) {
-            bus.fire(new ScrollEvent(lastX - screenX, lastY - screenY));
+		dragging = true;
 
-            lastX = screenX;
-            lastY = screenY;
-            return true;
-        }
+		if (Gdx.input.isButtonPressed(1)) {
+			bus.fire(new ScrollEvent(lastX - screenX, lastY - screenY));
 
-        return false;
-    }
+			lastX = screenX;
+			lastY = screenY;
+			return true;
+		}
+
+		return false;
+	}
 
 
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
 
-        lastX = screenX;
-        lastY = screenY;
+		lastX = screenX;
+		lastY = screenY;
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
-    }
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
+	}
 }
